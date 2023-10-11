6bedbbacfe1a6a1628d3ad87a3a09b01cfa4f16b
maroiane2 <devopscode9@gmail.com>
2023-10-11 23:55:31 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let myDiv = document.getElementById("my-div");
+function isTouchDevice() {
+    try{
+        document.createEvent(TouchEvent);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+
+const move = (e) =>{
+    try{
+        var x = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
+        var y = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
+    } catch(e){}
+
+    myDiv.style.left = x  + "px";
+    myDiv.style.top = y + "px";
+};
+
+document.addEventListener("mousemove", (e) => {
+    move(e);
+});
+document.addEventListener("touchmove", (e) => {
+    move(e);
+});
